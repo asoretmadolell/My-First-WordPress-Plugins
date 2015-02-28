c74af1520643b8dbce1786475910cd0cadf809ca
Alejandro Soret <soret_40@hotmail.com>
2015-02-28 16:28:05 +0100

    We've created a simple plugin to test filters. A filter is a type of hook (like actions), and it is used mainly to modify content before it is either shown to the user or written to the database. In this case, we've modified post titles, post content and category listing.

---
+++ wp-content/plugins/asm_title/asm_title.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Plugin Name: ASM Title
+ * Plugin URI: https://github.com/asoretmadolell
+ * Description: A very dumb plugin that modifies titles.
+ * Version: 1.0
+ * Author: Soret
+ * Author URI: https://github.com/asoretmadolell
+ * License: GPL2
+ */
+
+add_filter('the_title', 'asmtitle_title');
+add_filter('the_content', 'asmtitle_content');
+add_filter('list_cats', 'asmtitle_categories');
+
+/*
+ * Modify the title
+ */
+function asmtitle_title( $text )
+{
+    return 'OMG! ' . $text;
+}
+
+/*
+ * Modify the content
+ */
+function asmtitle_content( $text )
+{
+    return strtoupper( $text );
+}
+
+/*
+ * Modify categories
+ */
+function asmtitle_categories( $text )
+{
+    return strtolower( $text );
+}
