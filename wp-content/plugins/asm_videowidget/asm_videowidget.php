082e544c1bee0a065ca4f5395d50f377f79cfffc
Alejandro Soret <soret_40@hotmail.com>
2015-04-17 08:56:23 +0200

    To conclude the plugin created previously, we only have to create a new "WP_Widget" object, and then register it. Inside our new widget object, there are at least four functions that need to be called, which are the constructor method (which takes care of some settings), the "form" method (which takes care of the form of options to enter), the "update" method (which saves the widget form) and the "widget" method (which shows the widget in a post or page).

--- wp-content/plugins/asm_videowidget/asm_videowidget.php
+++ wp-content/plugins/asm_videowidget/asm_videowidget.php
@@ -31,7 +31,7 @@ function asm_youtube_handler()
 {
     // retrieve all metadata from the current post, using the global variable "$post"
     // http://codex.wordpress.org/Function_Reference/get_post_custom
-    $value = get_post_custom( $post->ID ); // returns a multidimensional array
+    $value = get_post_custom( $post->ID ); // returns a multidimensional array of all the custom fields
     $youtube_link = $value['asm_youtube'][0]; // first and only element of the array "asm_youtube", created when saving
     $youtube_link = esc_attr( $youtube_link ); // we'll do this when saving anyway, but it's good to make it a habit
     echo '<label for="asm_youtube">YouTube Video Link</label> <input type="text" id="asm_youtube" name="asm_youtube" value="' . $youtube_link . '" />';
@@ -66,4 +66,96 @@ function asm_save_metabox( $post_id )
         // "$meta_value" the value that the user entered (since it is a URL, it is good habit to escape it)
         update_post_meta( $post_id, 'asm_youtube', esc_url( $url ) ); 
     }
+}
+
+// register widgets
+// http://codex.wordpress.org/Widgets_API
+add_action('widgets_init', 'asmvw_widget_init');
+
+function asmvw_widget_init()
+{
+    register_widget( Asmvw_Widget );
+}
+
+/*
+ * Creation of our new widget object
+ */
+class Asmvw_Widget extends WP_Widget
+{
+    // constructor, which will take care of title, description, CSS class, etc
+    function Asmvw_Widget()
+    {
+        $widget_options = array(
+            'classname' => 'asmvw_class', // CSS class
+            'description' => 'Show a YouTube Video from a single post metadata' // description shown in widgets dashboard
+        );
+        
+        // "$id_base" is the CSS ID
+        // "$name" is the title shown in widgets dashboard
+        // "$widget_options" is the array that we've just defined above
+        $this->WP_Widget( 'asmvw_id', 'YouTube Video', $widget_options);
+    }
+    
+    // widget options form when adding it to a sidebar in the widgets dashboard
+    function form( $instance )
+    {
+        // first thing would be to assign some default values
+        $defaults = array(
+            'title' => 'Video'
+        );
+        
+        // add the defaults into our instance
+        // https://codex.wordpress.org/Function_Reference/wp_parse_args
+        $instance = wp_parse_args( (array)$instance, $defaults );
+        
+        // grab the title that the user entered
+        $title = esc_attr( $instance['title'] );
+        
+        // show the form
+        echo '<p>Title <input type="text" class="widefat" name="' . $this->get_field_name( 'title' ) . '" value="' . $title . '" /></p>';
+    }
+    
+    // process information entered by the user and save it to the database
+    function update( $new_instance, $old_instance )
+    {
+        $instance = $old_instance;
+        
+        $instance['title'] = strip_tags( $new_instance['title'] );
+        
+        return $instance;
+    }
+    
+    // show widget in post or page
+    function widget( $args, $instance )
+    {
+        // extract "$args" so that the values are available as variables within this scope
+        extract( $args );
+        
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        
+        // show widget only if it's a single post
+        if( is_single() )
+        {
+            echo $before_widget;
+            echo $before_title . $title . $after_title;
+            
+            // get post metadata
+            $asmvw_youtube = esc_url( get_post_meta( get_the_ID(), 'asm_youtube', true ) );
+            
+            // print widget content
+            echo '<iframe width="200" height="200" frameborder="0" allowfullscreen src="http://www.youtube.com/embed/' . get_youtube_id( $asmvw_youtube ) . '"></iframe>';
+            
+            echo $after_widget;
+        }
+    }
+}
+
+/*
+ * Get YouTube video ID from link
+ */
+// http://stackoverflow.com/questions/3392993/php-regex-to-get-youtube-video-id
+function get_youtube_id( $url )
+{
+    parse_str( parse_url( $url, PHP_URL_QUERY ), $my_array_of_vars );
+    return $my_array_of_vars['v'];
 }
