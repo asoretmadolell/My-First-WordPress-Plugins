1e4d04486a32cc91bdc1f94a7b3eae799f56cb2b
Alejandro Soret <soret_40@hotmail.com>
2015-05-12 20:06:45 +0200

    To conclude our plugin created previously, we will use it to create custom tables, as it is important to manage them depending on the plugin's lifecycle. On activation, we will check if our table exists, and create it if it doesn't. Then, we will filter the content and add its ID to our new database, along with the IP address and the date in which it was added. Finally, when uninstalling, we will also check if the table exists, and delete it if it does.

--- wp-content/plugins/asm_customtable/asm_customtable.php
+++ wp-content/plugins/asm_customtable/asm_customtable.php
@@ -15,7 +15,26 @@ register_activation_hook( __FILE__, 'asmct_create_update_table' );
 
 function asmct_create_update_table()
 {
-    error_log( 'plugin activated' );
+    // specify table name
+    global $wpdb;
+    $tablename = $wpdb->prefix . "asm_hits";
+    
+    // check if table exists, create it if it doesn't
+    if( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) != $tablename )
+    {
+        // construct the SQL query to create the table
+        $sql = "CREATE TABLE `$tablename` (
+            `hit_id`         INT( 11 ) NOT NULL AUTO_INCREMENT ,
+            `hit_ip`        VARCHAR( 100 ) NOT NULL ,
+            `hit_post_id`   INT( 11 ) NOT NULL ,
+            `hit_date`      DATETIME ,
+            PRIMARY KEY (hit_id)
+            );";
+        
+        // execute the query
+        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+        dbDelta( $sql );
+    }
 }
 
 // 2. Plugin deactivation
@@ -26,4 +45,38 @@ function asmct_deactivate()
     error_log( 'plugin deactivated' );
 }
 
-// 3. Plugin removal
+// filters the content of the post after it is retrieved, but before it is printed
+// https://codex.wordpress.org/Plugin_API/Filter_Reference/the_content
+add_action( 'the_content', 'asmct_save_hit' );
+
+function asmct_save_hit( $content )
+{
+    // check if it's a single post. if not, return the content as is
+    if( !is_single() )
+    {
+        return $content;
+    }
+    
+    // grab the post information
+    $post_id = get_the_ID();
+    
+    // grab the user's IP address
+    $ip = $_SERVER['REMOTE_ADDR'];
+    
+    // the same table name specified earlier
+    global $wpdb;
+    $tablename = $wpdb->prefix . "asm_hits";
+    
+    // array of the data to be inserted
+    $newdata = array(
+        'hit_ip' => $ip,
+        'hit_date' => current_time( 'mysql' ),
+        'hit_post_id' => $post_id
+    );
+    
+    // insert the data
+    $wpdb->insert( $tablename, $newdata );
+    
+    // remember, this is a filter, so you MUST return the content
+    return $content;
+}
--- wp-content/plugins/asm_customtable/uninstall.php
+++ wp-content/plugins/asm_customtable/uninstall.php
@@ -5,3 +5,16 @@ if( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 {
     exit();
 }
+
+// 3. Plugin removal
+    
+// the same table name specified earlier
+global $wpdb;
+$tablename = $wpdb->prefix . "asm_hits";
+
+// if the table exists, delete it
+if( $wpdb->get_var( "SHOW TABLES LIKE '$tablename'" ) == $tablename )
+{
+    $sql = "DROP TABLE `$tablename`;";
+    $wpdb->query( $sql );
+}
